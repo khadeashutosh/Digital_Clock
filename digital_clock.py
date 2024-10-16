de2b25e0495fb8e26157d341348f9e0cfff728c0
Ashu <ashutoshkhade2530@gmail.com>
2024-10-16 13:22:06 +0530

    completed

--- digital_clock.py
+++ digital_clock.py
@@ -1,1 +1,16 @@
-print("Hello world")
+import tkinter as tk
+from time import strftime
+
+root= tk.Tk()
+root.title("Digital Clock")
+def time():
+  current_time= strftime('%H:%M:%S %p \n %D')
+  label.config(text=current_time)
+  label.after(1000,time)
+
+label=tk.Label(root, font=('calibri', 50 , 'bold'), background='red', foreground='black')  
+label.pack(anchor='center')
+
+time()
+
+root.mainloop()
